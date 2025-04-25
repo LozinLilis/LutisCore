94bc878697d7a1c166d4c76e69dd26a2f57494fc
Lozin-Lilis <3531557655@qq.com>
2025-04-25 23:30:57 +0800

    增加遍历指定路径

--- src/main/java/org/lozin/tools/file/FileService.java
+++ src/main/java/org/lozin/tools/file/FileService.java
@@ -34,4 +34,17 @@ public class FileService {
 			}
 		}
 	}
+	public static List<String> getInferiorFiles(JavaPlugin plugin, String path) throws IOException {
+		File dataFolder = plugin.getDataFolder();
+		List<String> filePaths = new ArrayList<>();
+		try (Stream<Path> pathStream = Files.walk(dataFolder.toPath().resolve(path))) {
+			pathStream.filter(Files::isRegularFile)
+					.map(dataFolder.toPath()::relativize) // 转换为插件目录的相对路径
+					.map(p -> p.toString().replace("\\", "/")) // 强制统一为UNIX路径
+					.forEach(filePaths::add);
+		}catch (IOException e) {
+			plugin.getLogger().warning("无法获取插件文件列表：" + e.getMessage());
+		}
+		return filePaths;
+	}
 }
